7d4bb8aa7d341f904847178652fd1a484b5389b8
Mitchell Valine <mitchellvaline@yahoo.com>
2015-05-21 18:23:27 -0700

    seperated rounds into seperate file in routes folder, require in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var rounds = require('./routes/rounds');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/rounds', rounds);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
