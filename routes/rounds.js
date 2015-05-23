1bfaf4d36408ab892e9cba3141a8f7596c861480
Mitchell Valine <mitchellvaline@gmail.com>
2015-05-22 21:18:14 -0700

    reformatted rounds.js

--- routes/rounds.js
+++ routes/rounds.js
@@ -111,13 +111,20 @@ router.put('/:game_id', function(req, res) {
     var id = req.params.game_id;
 
     // Grab data from http request
-    var data = {complete: req.body.complete, total_item_value: req.body.total_item_value, total_num_items: req.body.total_num_items, item_witheld: req.body.item_witheld, players: req.body.players};
+    var data = {complete: req.body.complete,
+        total_item_value: req.body.total_item_value,
+        total_num_items: req.body.total_num_items,
+        item_witheld: req.body.item_witheld,
+        players: req.body.players
+    };
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
 
         // SQL Query > Update Data
-        client.query("UPDATE rounds SET complete=($1), total_item_value=($2), total_num_items=($3), item_witheld=($4), players=($5) WHERE game_id=($6)", [data.complete, data.total_item_value, data.total_num_items, data.item_witheld, data.players, id]);
+        client.query("UPDATE rounds SET complete=($1), total_item_value=($2), total_num_items=($3), item_witheld=($4), players=($5) WHERE game_id=($6)",
+            [data.complete, data.total_item_value, data.total_num_items, data.item_witheld, data.players, id]
+        );
 
         // SQL Query > Select Data
         var query = client.query("SELECT * FROM rounds WHERE game_id=($1)", id);
