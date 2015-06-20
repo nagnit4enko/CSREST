840459c13332977e58d4fee350ba2dfbb353c642
Mitchell Valine <mitchellvaline@yahoo.com>
2015-06-20 13:14:33 -0700

    Changed inventory route GET from http to npm request

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,39 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var request = require('request');
 
 // Steam CSGO inventory url 'http://steamcommunity.com/profiles/<STEAMID>/inventory/json/730/2'
 
 /* GET inventory */
 router.get('/:steam_id', function(req, res) {
-    var results = [];
-
-    var apiKey = 'C1797373C07E4F2F658C1E6E2C2679D4';
     var id = req.params.steam_id;
-    var pathParams = '/IEconItems_730/GetPlayerItems/v1/?key=' + apiKey + '&format=json&steamid=' + id;
+    var connectionString = 'http://steamcommunity.com/profiles/' + id + '/inventory/json/730/2';
 
-    //var connectionString = 'http://steamcommunity.com/profiles/' + id + '/inventory/json/730/2';
-
-    var options = {
-        host: 'api.steampowered.com',
-        path: pathParams,
-        method: 'GET'
-    };
-
-    http.get(options, function(response) {
-        var body = '';
-        response.on('data', function(chunk) {
-            body += chunk;
-        });
-        response.on('end', function() {
-            var results = JSON.parse(body);
-            return res.json(results);
-        });
-        response.on('error', function(err) {
-            console.log(err.message);
-            return err;
-        })
-    });
+    request({
+      uri: connectionString
+    }, function(error, response, body) {
+      res.send(body);
+    })
 
 });
 
