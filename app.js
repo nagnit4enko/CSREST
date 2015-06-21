653275e74b8f602c6e6ce74d9e02a4d7d3d24e52
Mitchell Valine <mitchellvaline@yahoo.com>
2015-06-20 18:18:04 -0700

    fixed merge conflict

--- app.js
+++ app.js
@@ -34,67 +34,7 @@ app.use('/', routes);
 app.use('/api/rounds', rounds);
 app.use('/api/users', users);
 app.use('/api/inventory', inventory);
-<<<<<<< HEAD
-
-
-/// Steam bot account log on
-var bot = new Steam.SteamClient();
-var offers = new SteamTradeOffers();
-
-var logOnOptions = {
-  accountName: process.env.STEAM_USERNAME,
-  password: process.env.STEAM_PASSWORD,
-};
-
-var authCode = 'F6PKC';
-
-if (require('fs').existsSync('sentryfile')) {
-  logOnOptions['shaSentryfile'] = require('fs').readFileSync('sentryfile');
-} else if (authCode != '') {
-  logOnOptions['authCode'] = authCode;
-}
-
-bot.logOn(logOnOptions);
-
-bot.on('loggedOn', function() {
-  console.log('logged in');
-  bot.setPersonaState(Steam.EPersonaState.Online);
-});
-
-bot.on('sentry',function(sentryHash) {
-  require('fs').writeFile('sentryfile',sentryHash,function(err) {
-    if(err){
-      console.log(err);
-    }
-    else {
-      console.log('Saved sentry file hash as "sentryfile"');
-    }
-  });
-});
-
-bot.on('chatInvite', function(chatRoomID, chatRoomName, patronID) {
-  console.log('Got an invite to ' + chatRoomName + ' from ' + bot.users[patronID].playerName);
-  bot.joinChat(chatRoomID); // autojoin on invite
-});
-
-bot.on('message', function(source, message, type, chatter) {
-  // respond to both chat room and private messages
-  console.log('Received message: ' + message);
-  bot.sendMessage(source, 'Autoresponse from Bot', Steam.EChatEntryType.ChatMsg); // ChatMsg by default
-});
-
-bot.on('webSessionID', function(sessionID) {
-  bot.webLogOn(function(newCookie) {
-    offers.setup( {
-      sessionID: sessionID,
-      webCookie: newCookie
-    });
-  });
-});
-
-=======
 app.use('/api/deposit', deposit);
->>>>>>> tradeoffers
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
