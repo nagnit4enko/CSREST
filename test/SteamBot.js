ba073f4116c7f9bd5eed660513782605f5d0ed94
Mitchell Valine <mitchellvaline@yahoo.com>
2015-07-25 14:08:26 -0700

    LogOn test removed for now

--- test/SteamBot.js
+++ test/SteamBot.js
@@ -6,16 +6,15 @@ import SteamTradeOffers from 'steam-tradeoffers';
 import SteamBot from '../modules/SteamBot.js';
 
 describe('Steam Bot', function() {
-
-  describe('Steam Log On', function() {
-    it('should log on user using credentials from environment variables', function(done) {
-      this.timeout(5000);
-      SteamBot.LogOn();
-      SteamBot.steamClient.on('logOnResponse', function(res) {
-        res.eresult.should.equal(Steam.EResult.OK);
-        SteamBot.steamClient.loggedOn.should.be.true;
-        done();
-      });
-    });
-  });
+  // describe('Steam Log On', function() {
+  //   it('should log on user using credentials from environment variables', function(done) {
+  //     this.timeout(5000);
+  //     SteamBot.LogOn();
+  //     SteamBot.steamClient.on('logOnResponse', function(res) {
+  //       res.eresult.should.equal(Steam.EResult.OK);
+  //       SteamBot.steamClient.loggedOn.should.be.true;
+  //       done();
+  //     });
+  //   });
+  // });
 });
